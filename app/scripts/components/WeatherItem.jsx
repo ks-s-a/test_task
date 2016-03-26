ae1e1d72d96bd28116c8b714636f00035fe79640
Sergey Ksenofontov <s.ksenofontov@sup.com>
2016-03-26 19:04:43 +0300

    add pressure and humidity

--- app/scripts/components/WeatherItem.jsx
+++ app/scripts/components/WeatherItem.jsx
@@ -5,7 +5,6 @@ function WeatherItem(props) {
   moment.locale('ru');
   const dateStr = moment(props.dt * 1000).format('LL').slice(0, -8);
 
-
   return <li key={props.key} className="weather-item" >
     <div className="weather-item__date" >
       <span className="weather-item__day">{dateStr}</span>
@@ -13,9 +12,15 @@ function WeatherItem(props) {
     <div className="weather-item__image" >
       <img src={`http://openweathermap.org/img/w/${props.weather[0].icon}.png`} />
     </div>
-    <div className="weather-item__temp" >
+    <div className="weather-item__prop">
       <span>{Math.round(props.main.temp)}</span>
       <span> °</span>
+    </div>
+    <div className="weather-item__prop">
+      <span>{props.main.humidity} %</span>
+    </div>
+    <div className="weather-item__prop">
+      <span>{Math.round((props.main.pressure * 100) / 133.322)} мм</span>
     </div>
   </li>;
 }
